ca2f0da69193f3f9deffcf86005ea5b56b55acdb
welintondeveloper <welintondeveloper@gmail.com>
2024-05-22 12:44:05 -0300

    manipulando strings

---
+++ dia1.py
@@ -0,0 +1,8 @@
+#a varias maneiras de usar símbolos ou caracteres especiais dentro das strings sem que o compilador se confunda
+print("A 'single quote' inside a double quote")#basta usar aspas diferentes na abertura e fechamento da string
+print('A "double quote" inside a single quote')#pode ser com aspas duplas ou simples mais tem que diferenciar
+print("Alternatively you can just \"escape\" the quote")
+#ou usar barra invertida como na linha acima, o compilador entenderá que trata-se de um caractere e não um comando
+
+
+

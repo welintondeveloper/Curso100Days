6fa67628d2b3e9a642c79de07277d217bcec5552
welintondeveloper <welintondeveloper@gmail.com>
2024-05-22 13:36:28 -0300

    Manipulando Strings 2

--- dia1.py
+++ dia1.py
@@ -1,8 +1,17 @@
+#___________________________________________Manipulando Strings_________________________________________________#
+
 #a varias maneiras de usar símbolos ou caracteres especiais dentro das strings sem que o compilador se confunda
 print("A 'single quote' inside a double quote")#basta usar aspas diferentes na abertura e fechamento da string
 print('A "double quote" inside a single quote')#pode ser com aspas duplas ou simples mais tem que diferenciar
 print("Alternatively you can just \"escape\" the quote")
 #ou usar barra invertida como na linha acima, o compilador entenderá que trata-se de um caractere e não um comando
 
+#___________________________________________Manipulando Strings 1_______________________________________________#
 
-
+#maneira comun de imprimir varias linhas usando a função print para imprimir
+print("oi qual seu nome")
+print("tudo bem como você")
+print("também estou bem obrigado")
+#maneira mais facil de quebrar linha sem precisar criar varias funçoes de print toda ves
+print(" oi qual seu nome\n tudo bem como você\n também estou bem obrigado")
+#___________________________________________Manipulando Strings 2_______________________________________________#

93f0d51d1d50d1ea346328a8812d38514e6e67cc
welintondeveloper <welintondeveloper@gmail.com>
2024-05-25 23:30:56 -0300

    Manipulando Variáveis

---
+++ exercicio_Programa_Nomes_Para_Bandas.py
@@ -0,0 +1,7 @@
+print("Ben vindo ao gerador de nome de bandas!\n")
+nomePet = input("Qual Nome do seu animal de estimaçao\n")
+nomeCidade = input("Qual nome da sua cidade")
+# se quiser pode primeiro criar uma variavel concatenando as duas variáveis
+# nome_Banda = nomePet + " " + nomeCidade
+# print(f"um bom nome para sua banda é: {nome_Banda}")
+print("um bom nome para sua banda é: "+nomeCidade+" "+nomePet)

a0d27176547f4fa85626b80ad9647d02bab2ad72
welintondeveloper <welintondeveloper@gmail.com>
2024-05-25 22:50:01 -0300

    Manipulando Variáveis

---
+++ Dia 1 Manipulando Variáveis.py
@@ -0,0 +1,7 @@
+#________________________________________ Manipulando variáveis __________________________________________________#
+# para uma boa pratica sempre use nomes de variáveis que fação sentido para quando outro programador ou ate você
+# mesmo for ler elas futuramente faça  sentido use por exemplo longitude = 100 ao invés de somente l=100
+# em python não é possível usar um nome de variável que comece com um numero ex: 1litro = 1000
+# também não e uma boa pratica usar nomes de funções nas suas variáveis Ex:
+# print = "oi" pode  até funcionar mais também pode confundir o compilador
+# se quiser usar um nome que faça sentido você pode usar _ ou camel case Ex: linha_central ou LinhaCentral

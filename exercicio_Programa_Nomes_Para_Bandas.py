2c57fb3f34fb7cfbd53105dfb8c8a33872873e5d
welintondeveloper <welintondeveloper@gmail.com>
2024-05-26 00:52:54 -0300

    Programa Gerador De Nome De Bandas

--- exercicio_Programa_Nomes_Para_Bandas.py
+++ exercicio_Programa_Nomes_Para_Bandas.py
@@ -1,6 +1,6 @@
 print("Ben vindo ao gerador de nome de bandas!\n")
 nomePet = input("Qual Nome do seu animal de estimaçao\n")
-nomeCidade = input("Qual nome da sua cidade")
+nomeCidade = input("Qual nome da sua cidade\n")
 # se quiser pode primeiro criar uma variavel concatenando as duas variáveis
 # nome_Banda = nomePet + " " + nomeCidade
 # print(f"um bom nome para sua banda é: {nome_Banda}")

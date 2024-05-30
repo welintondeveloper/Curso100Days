2f339b08373e01c975ea49920cfe8e3f341c6561
welintondeveloper <welintondeveloper@gmail.com>
2024-05-30 16:45:00 -0300

    Programa verifica se o número é par ou impar

---
+++ Programa verifica se o número e par ou impar.py
@@ -0,0 +1,18 @@
+#meu programa
+
+number = int(input("digite um numero inteiro para ver se e par ou impar\n"))
+if number % 2 == 0:
+  print("este número é par")
+else:
+  print("este número e ímpar")
+
+# # programa do curso
+# # Which number do you want to check?
+# number = int(input("digite um numero inteiro para ver se e par ou impar\n"))
+# # 🚨 Don't change the code above 👆
+
+# # Write your code below this line 👇
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")

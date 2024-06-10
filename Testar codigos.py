688bfa703a4b4df4b84911357b61a7e98daab8cf
welintondeveloper <welintondeveloper@gmail.com>
2024-06-10 17:06:34 -0300

    programa ilha do tesouro

---
+++ Testar codigos.py
@@ -0,0 +1,12 @@
+# first_digit = int(10)
+# second_digit = int(5)
+# score = int(str(first_digit) + str(second_digit))
+# print(score)
+
+# numero_inteiro = 42
+# numero_string = str(numero_inteiro)
+# print(numero_string)  # Isso imprimirá "42"
+
+# palavra = "ExEmPLO"
+# palavra_minuscula = palavra.upper()
+# print(palavra_minuscula)  # Saída: exemplo

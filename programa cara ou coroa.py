88beac3f59cb1ed76adbe84ade84426f685792d3
welintondeveloper <welintondeveloper@gmail.com>
2024-06-12 08:35:39 -0300

    Programa Cara ou Coroa

---
+++ programa cara ou coroa.py
@@ -0,0 +1,21 @@
+# Meu programa---------------------------------------------------------
+import random
+
+
+HeadsTails = random.randint(0, 1)
+
+if HeadsTails == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+#programa do curso-----------------------------------------------------
+
+# import random
+
+
+# random_side = random.randint(0, 1)
+# if random_side == 1:
+#   print("Heads")
+# else:
+#   print("Tails")
